c25cff3be75b0f987f6bd8b2eec3b914799e0d64
lrobinsonco <lrobinsonco@gmail.com>
2017-05-11 15:16:11 -0600

    master?

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('../db/knex')
+var pg = require('../db/knex');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   pg('book').select()
   .then(books => {
-    res.render('list_books', { books })
-  })
+    res.render('list_books', {books});
+  });
 });
 
 module.exports = router;
